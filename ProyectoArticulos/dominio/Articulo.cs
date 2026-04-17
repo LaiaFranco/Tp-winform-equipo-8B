f14243119609a36fc934c435d1e61a7b0746a168
Laia <deniseef.9@gmail.com>
2026-04-17 16:26:39 -0300

    Pimera clase Articulo y clase imagen

---
+++ ProyectoArticulos/dominio/Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        public string CódigoDeArtculo { get; set;  }
+        public string Nombre { get; set; }
+        public string Descripción { get; set; } 
+        public int Marca { get; set;  } //(seleccionable de una lista desplegable).
+        public int Categoría{ get; set; }   //(seleccionable de una lista desplegable.
+        public Imagen imagen { get; set; }
+        public decimal Precio { get; set;  }
+    }
+}
