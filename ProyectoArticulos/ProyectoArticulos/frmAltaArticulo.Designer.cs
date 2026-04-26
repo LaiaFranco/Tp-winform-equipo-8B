0a9caf74a4b6a2a905db0b03f435df68975db844
SebastianDlaCruz <sebastiandelacruz335@linktic.com>
2026-04-26 14:34:18 -0300

    articulo restaurado

---
+++ ProyectoArticulos/ProyectoArticulos/frmAltaArticulo.Designer.cs
@@ -0,0 +1,348 @@
+﻿namespace ProyectoArticulos
+{
+    partial class frmAltaArticulo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.lblMarca = new System.Windows.Forms.Label();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.cmbCategoria = new System.Windows.Forms.ComboBox();
+            this.cmbNMarca = new System.Windows.Forms.ComboBox();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.lblUrlImagen = new System.Windows.Forms.Label();
+            this.txtURLImagen = new System.Windows.Forms.TextBox();
+            this.btnAgregarImagen = new System.Windows.Forms.Button();
+            this.pbxImagen = new System.Windows.Forms.PictureBox();
+            this.lblExplicacion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.Location = new System.Drawing.Point(114, 511);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(109, 40);
+            this.btnGuardar.TabIndex = 0;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = true;
+            this.btnGuardar.Click += new System.EventHandler(this.btnGuardar_Click);
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(211, 55);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(128, 26);
+            this.lblTitulo.TabIndex = 1;
+            this.lblTitulo.Text = " ARTICULO";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(77, 129);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(67, 20);
+            this.lblCodigo.TabIndex = 2;
+            this.lblCodigo.Text = "Codigo: ";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(83, 382);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(61, 20);
+            this.lblPrecio.TabIndex = 3;
+            this.lblPrecio.Text = "Precio: ";
+            this.lblPrecio.Click += new System.EventHandler(this.lblPrecio_Click);
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(58, 320);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(86, 20);
+            this.lblCategoria.TabIndex = 4;
+            this.lblCategoria.Text = "Categoria: ";
+            // 
+            // lblMarca
+            // 
+            this.lblMarca.AutoSize = true;
+            this.lblMarca.Location = new System.Drawing.Point(83, 269);
+            this.lblMarca.Name = "lblMarca";
+            this.lblMarca.Size = new System.Drawing.Size(61, 20);
+            this.lblMarca.TabIndex = 5;
+            this.lblMarca.Text = "Marca: ";
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(48, 436);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(96, 20);
+            this.lblDescripcion.TabIndex = 6;
+            this.lblDescripcion.Text = "Descripcion:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(71, 180);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(73, 20);
+            this.lblNombre.TabIndex = 7;
+            this.lblNombre.Text = "Nombre: ";
+            // 
+            // cmbCategoria
+            // 
+            this.cmbCategoria.FormattingEnabled = true;
+            this.cmbCategoria.Location = new System.Drawing.Point(170, 320);
+            this.cmbCategoria.Name = "cmbCategoria";
+            this.cmbCategoria.Size = new System.Drawing.Size(260, 28);
+            this.cmbCategoria.TabIndex = 8;
+            // 
+            // cmbNMarca
+            // 
+            this.cmbNMarca.FormattingEnabled = true;
+            this.cmbNMarca.Location = new System.Drawing.Point(170, 269);
+            this.cmbNMarca.Name = "cmbNMarca";
+            this.cmbNMarca.Size = new System.Drawing.Size(260, 28);
+            this.cmbNMarca.TabIndex = 9;
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(170, 129);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(260, 26);
+            this.txtCodigo.TabIndex = 10;
+            this.txtCodigo.TextChanged += new System.EventHandler(this.txtCodigo_TextChanged);
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(170, 436);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(260, 26);
+            this.txtDescripcion.TabIndex = 11;
+            this.txtDescripcion.TextChanged += new System.EventHandler(this.txtDescripcion_TextChanged);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(170, 180);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(260, 26);
+            this.txtNombre.TabIndex = 12;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(170, 376);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(260, 26);
+            this.txtPrecio.TabIndex = 13;
+            this.txtPrecio.TextChanged += new System.EventHandler(this.txtPrecio_TextChanged);
+            this.txtPrecio.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrecio_KeyPress);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(297, 511);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(109, 40);
+            this.btnCancelar.TabIndex = 14;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // lblUrlImagen
+            // 
+            this.lblUrlImagen.AutoSize = true;
+            this.lblUrlImagen.Location = new System.Drawing.Point(36, 228);
+            this.lblUrlImagen.Name = "lblUrlImagen";
+            this.lblUrlImagen.Size = new System.Drawing.Size(108, 20);
+            this.lblUrlImagen.TabIndex = 15;
+            this.lblUrlImagen.Text = "URL Imagen: ";
+            // 
+            // txtURLImagen
+            // 
+            this.txtURLImagen.Location = new System.Drawing.Point(170, 228);
+            this.txtURLImagen.Name = "txtURLImagen";
+            this.txtURLImagen.Size = new System.Drawing.Size(260, 26);
+            this.txtURLImagen.TabIndex = 16;
+            this.txtURLImagen.Leave += new System.EventHandler(this.txtURLImagen_Leave);
+            // 
+            // btnAgregarImagen
+            // 
+            this.btnAgregarImagen.Location = new System.Drawing.Point(445, 225);
+            this.btnAgregarImagen.Name = "btnAgregarImagen";
+            this.btnAgregarImagen.Size = new System.Drawing.Size(32, 32);
+            this.btnAgregarImagen.TabIndex = 17;
+            this.btnAgregarImagen.Text = "+";
+            this.btnAgregarImagen.UseVisualStyleBackColor = true;
+            this.btnAgregarImagen.Click += new System.EventHandler(this.btnAgregarImagen_Click);
+            // 
+            // pbxImagen
+            // 
+            this.pbxImagen.Location = new System.Drawing.Point(533, 133);
+            this.pbxImagen.Name = "pbxImagen";
+            this.pbxImagen.Size = new System.Drawing.Size(339, 303);
+            this.pbxImagen.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxImagen.TabIndex = 18;
+            this.pbxImagen.TabStop = false;
+            // 
+            // lblExplicacion
+            // 
+            this.lblExplicacion.AutoSize = true;
+            this.lblExplicacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblExplicacion.ForeColor = System.Drawing.Color.Red;
+            this.lblExplicacion.Location = new System.Drawing.Point(167, 98);
+            this.lblExplicacion.Name = "lblExplicacion";
+            this.lblExplicacion.Size = new System.Drawing.Size(195, 15);
+            this.lblExplicacion.TabIndex = 19;
+            this.lblExplicacion.Text = "Los campos con * son obligatorios";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(149, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(15, 20);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "*";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(150, 436);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(15, 20);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "*";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(149, 379);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 20);
+            this.label3.TabIndex = 22;
+            this.label3.Text = "*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(150, 186);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(15, 20);
+            this.label5.TabIndex = 24;
+            this.label5.Text = "*";
+            // 
+            // frmAltaArticulo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(924, 597);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblExplicacion);
+            this.Controls.Add(this.pbxImagen);
+            this.Controls.Add(this.btnAgregarImagen);
+            this.Controls.Add(this.txtURLImagen);
+            this.Controls.Add(this.lblUrlImagen);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.cmbNMarca);
+            this.Controls.Add(this.cmbCategoria);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.lblMarca);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.btnGuardar);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmAltaArticulo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Agregar Articulo";
+            this.Load += new System.EventHandler(this.frmAltaArticulo_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbxImagen)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.Label lblMarca;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.ComboBox cmbCategoria;
+        private System.Windows.Forms.ComboBox cmbNMarca;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Label lblUrlImagen;
+        private System.Windows.Forms.TextBox txtURLImagen;
+        private System.Windows.Forms.Button btnAgregarImagen;
+        private System.Windows.Forms.PictureBox pbxImagen;
+        private System.Windows.Forms.Label lblExplicacion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ ProyectoArticulos/ProyectoArticulos/frmAltaArticulo.cs
@@ -0,0 +1,236 @@
+﻿using dominio;
+using negocio; 
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace ProyectoArticulos
+{
+    public partial class frmAltaArticulo : Form
+    {
+        private Articulo articulo = null;
+        private OpenFileDialog archivo = null;
+        public frmAltaArticulo()
+        {
+            InitializeComponent();
+        }
+        public frmAltaArticulo(Articulo articulo)
+        {
+            InitializeComponent();
+            this.articulo = articulo;
+            Text = "Modificar Articulo";
+        }
+        private void lblPrecio_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Close(); 
+        }
+
+        private void frmAltaArticulo_Load(object sender, EventArgs e)
+        {
+            EnableButton(); 
+            cargarComboMarca();
+            cargarComboCategoria();
+
+            ArticuloNegocio artNegocio = new ArticuloNegocio();
+            MarcaNegocio marcaNegocio = new MarcaNegocio();
+            CategoriaNegocio catNegocio = new CategoriaNegocio();
+            try
+            {
+                cmbNMarca.DataSource = marcaNegocio.listar();
+                cmbNMarca.ValueMember = "Id";
+                cmbNMarca.DisplayMember = "Descripcion";
+
+                cmbCategoria.DataSource = catNegocio.listar();
+                cmbCategoria.ValueMember = "Id";
+                cmbCategoria.DisplayMember = "Descripcion";
+
+                if (articulo != null)
+                {
+                    txtCodigo.Text = articulo.CodigoDeArtculo.ToString();
+                    txtNombre.Text = articulo.Nombre.ToString();
+                    txtDescripcion.Text = articulo.Descripcion.ToString();
+                    txtURLImagen.Text = articulo.Imagen.UrlImagen;
+                    cargarImagen(articulo.Imagen.UrlImagen);
+                    cmbNMarca.SelectedValue = articulo.Marca.Id;
+                    cmbCategoria.SelectedValue = articulo.Categoria.Id;
+                    txtPrecio.Text = articulo.Precio.ToString(); 
+                  
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+        private void cargarComboMarca()
+        {
+            MarcaNegocio negocio = new MarcaNegocio(); 
+            cmbNMarca.DataSource = negocio.listar();
+            cmbNMarca.DisplayMember = "Descripcion";
+            cmbNMarca.ValueMember = "Id"; 
+        }
+
+        private void cargarComboCategoria()
+        {
+            CategoriaNegocio negocio = new CategoriaNegocio();
+            cmbCategoria.DataSource = negocio.listar();
+            cmbCategoria.DisplayMember = "Descripcion";
+            cmbCategoria.ValueMember = "Id"; 
+        }
+
+        private void txtPrecio_TextChanged(object sender, EventArgs e)
+        {
+            EnableButton(); 
+        }
+
+        private void txtPrecio_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (char.IsDigit(e.KeyChar))
+                return;
+
+            // Permitir backspace
+            if (e.KeyChar == (char)8)
+                return;
+
+            // Permitir un solo separador decimal (coma o punto)
+            if ((e.KeyChar == ',' || e.KeyChar == '.') &&
+                !txtPrecio.Text.Contains(",") &&
+                !txtPrecio.Text.Contains("."))
+                return;
+
+            // Bloquear todo lo demás
+            e.Handled = true;
+        }
+        private void EnableButton()
+        {
+            btnGuardar.Enabled =
+            txtNombre.Text != "" &&
+            txtCodigo.Text != "" &&
+            txtDescripcion.Text != "" &&
+            txtPrecio.Text != "";
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            ArticuloNegocio negocio = new ArticuloNegocio();
+            try
+            {
+                if (articulo == null) 
+                    articulo = new Articulo();
+
+                articulo.CodigoDeArtculo = txtCodigo.Text.ToUpper();
+                articulo.Nombre = txtNombre.Text;
+                articulo.Descripcion = txtDescripcion.Text;
+
+                if (articulo.Imagen == null)
+                    articulo.Imagen = new Imagen(); 
+                articulo.Imagen.UrlImagen = txtURLImagen.Text;
+                articulo.Marca = (Marca)cmbNMarca.SelectedItem;
+                articulo.Categoria = (Categoria)cmbCategoria.SelectedItem;
+               
+                    articulo.Precio = decimal.Parse(txtPrecio.Text);
+                
+                
+
+                if (articulo.Id != 0)
+                {
+                    negocio.Modificar(articulo);
+                    MessageBox.Show("Modificado exitosamente");
+                }
+                else
+                {
+
+                  
+                        negocio.Agregar(articulo);
+
+                        AccesoDatos datos = new AccesoDatos();
+                        datos.setearConsulta("SELECT MAX(Id) FROM ARTICULOS");
+                        int idArt = (int)datos.EjecutarScalar();
+
+                        if (!string.IsNullOrEmpty(txtURLImagen.Text))
+                        {
+                            AccesoDatos datosImg = new AccesoDatos();
+                            datosImg.setearConsulta("INSERT INTO IMAGENES (IdArticulo, ImagenUrl) VALUES (@idArticulo, @url)");
+                            datosImg.setearParametro("@idArticulo", idArt);
+                            datosImg.setearParametro("@url", txtURLImagen.Text);
+                            datosImg.ejecutarAccion();
+                        }
+
+                    MessageBox.Show("Agregado exitosamente");
+                }
+           
+                Close(); 
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                Close();
+
+            }
+        }
+
+        private void cargarImagen(string imagen)
+        {
+            try
+            {
+                pbxImagen.Load(imagen);
+            }
+            catch (Exception)
+            {
+               pbxImagen.Load("https://efectocolibri.com/wp-content/uploads/2021/01/placeholder.png");
+            }
+        }
+
+        private void btnAgregarImagen_Click(object sender, EventArgs e)
+        {
+            archivo = new OpenFileDialog();
+            archivo.Filter = "jpg|*.jpg;|png|*.png";
+            if (archivo.ShowDialog() == DialogResult.OK)
+            {
+                txtURLImagen.Text = archivo.FileName;
+                cargarImagen(archivo.FileName);
+
+                //guardo la imagen
+                //File.Copy(archivo.FileName, ConfigurationManager.AppSettings["images-folder"] + archivo.SafeFileName);
+            }
+
+        }
+
+        private void txtURLImagen_Leave(object sender, EventArgs e)
+        {
+            cargarImagen(txtURLImagen.Text);
+        }
+
+        private void txtCodigo_TextChanged(object sender, EventArgs e)
+        {
+            EnableButton(); 
+
+        }
+
+        private void txtNombre_TextChanged(object sender, EventArgs e)
+        {
+            EnableButton(); 
+        }
+
+        private void txtDescripcion_TextChanged(object sender, EventArgs e)
+        {
+            EnableButton(); 
+        }
+    }
+}
